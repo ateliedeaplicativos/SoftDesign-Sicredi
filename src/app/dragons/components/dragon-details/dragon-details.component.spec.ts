1da58069b931e26b5231f8dcab12788174936305
Vini Azevedo <ateliedeaplicativos@gmail.com>
2022-01-04 23:54:29 -0300

    Atualização extra

--- src/app/dragons/components/dragon-details/dragon-details.component.spec.ts
+++ src/app/dragons/components/dragon-details/dragon-details.component.spec.ts
@@ -3,12 +3,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { Dragon } from '../../models/dragon';
 import { DragonDetailsComponent } from './dragon-details.component';
+import { of } from 'rxjs';
+
 import { ActivatedRoute } from '@angular/router';
 
 describe('DragonDetailsComponent', () => {
   let component: DragonDetailsComponent;
-  let fixture: ComponentFixture<DragonDetailsComponent>;
-
+    let fixture: ComponentFixture<DragonDetailsComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -17,26 +18,34 @@ describe('DragonDetailsComponent', () => {
       ],
       providers:[],
       declarations: [ DragonDetailsComponent ]
-    })
-    .compileComponents();
-  });
-
+      })
+      .compileComponents();
+    });
   beforeEach(() => {
     fixture = TestBed.createComponent(DragonDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   it('should create', () => {
     expect(component).toBeTruthy();
-    expect(component.dragon).toEqual(new Dragon());
-  });
-
+      expect(component.dragon).toEqual(new Dragon());
+    });
   it('constructor - need to have correct values', () => {
     expect(component['activatedRoute'] instanceof ActivatedRoute).toBeTruthy();
-  });
-
+    });
   it('onInit - need to get correct value', () => {
-        component.ngOnInit();
+    let _dataMock = {
+    dragon: {
+      id: '1',
+      name: 'dragon of fire',
+      type: 'fire',
+      createdAt: '2021-01-01',
+    }
+    }
+   console.log(component);
+   //Forcing return
+  component['activatedRoute'].data = of(_dataMock);
+  component.ngOnInit();
+  expect(component.dragon).toEqual(new Dragon(_dataMock.dragon));
   });
 });
